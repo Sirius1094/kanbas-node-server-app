08df3f436767546a5dd24f0d078c775ce7e05341
Sirius1094 <liuteng@njust.edu.cn>
2024-08-03 23:22:16 -0700

    first commit

---
+++ Kanbas/Database/index.js
@@ -0,0 +1,15 @@
+import courses from "./courses.js";
+import modules from "./modules.js";
+import users from "./users.js";
+import assignments from "./assignments.js";
+import grades from "./grades.js";
+import enrollments from "./enrollments.js";
+
+export default {
+    courses,
+    modules,
+    users,
+    assignments,
+    grades,
+    enrollments
+}

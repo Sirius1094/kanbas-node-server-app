f0cf6e269fc8ff3454d024bcf243b20103aa294f
Sirius1094 <liuteng@njust.edu.cn>
2024-08-13 22:15:37 -0700

    update a5

--- App.js
+++ App.js
@@ -5,12 +5,16 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import PathParameters from "./Lab5/PathParameters.js";
+import QueryParameters from "./Lab5/QueryParameters.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 Hello(app);
 Lab5(app);
+PathParameters(app);
+QueryParameters(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
